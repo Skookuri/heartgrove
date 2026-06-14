de1daa9015cc603440425b20b5a0dc6c04520167
Skookuri <alyssa.williams.tk360@gmail.com>
2026-06-14 01:58:49 -0400

    fixed polishing

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -1,7 +1,7 @@
 import logo from "/src/images/MW_Logo.png";
 import hgBg from "/src/images/alone-martina-stipan.jpg";
-import spaceAntsBg from "/src/images/espacio.jpg";
-import bobBg from "/src/images/ferre.jpg";
+// import spaceAntsBg from "/src/images/espacio.jpg";
+// import bobBg from "/src/images/ferre.jpg";
 
 import { GlobalStyles } from "./GlobalStyles";
 import NavBar from "./NavBar";
